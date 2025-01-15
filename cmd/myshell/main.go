7a0d86cd2757a9c9f05bead29c64e9f8508c2588
Apoorv221 <apoorvmathur471@gmail.com>
2025-01-15 19:43:22 +0530

    cd code push.

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -84,7 +84,7 @@ func DoPwd(){
 }
 
 func DoCd(params []string){
-	err := os.Chdir(params)
+	err := os.Chdir(params[0])
 	if err!=nil{
 		fmt.Fprintln(os.Stdout,"cd: %s: No such file or directory",params)
 	}
